e133d1ffaeb63fcecafe8ae822b4ad077ab0e378
Anomig <goyvaerts.naomi@hotmail.com>
2024-11-18 22:30:46 +0100

    fix: juiste link mongodb

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect('mongodb://localhost:27017/');
+        const conn = await mongoose.connect('mongodb+srv://r0677456:2t7GtLriVisd8ZF1@sneaker.fxq0g.mongodb.net/Sneakershop');
         console.log('MongoDB Connected: ${conn.connection.host}');
     }catch (err){
         console.error(err);
