1ccfc8f385379c95fb3108121a54333241a23af6
IsabelDH <isabel.de.haes@hotmail.com>
2024-11-24 14:15:24 +0100

    Feat: fixed again

--- app.js
+++ app.js
@@ -38,12 +38,6 @@ app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/products", productsRouter);
 app.use("/api/v1/cart", cartRouter);
 
-// Serve Vue app for all other routes
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
