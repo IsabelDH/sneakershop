59f4cdf6d2416dacd763ad2d7d9c748ec8304892
IsabelDH <isabel.de.haes@hotmail.com>
2024-11-26 13:00:32 +0100

    feat: added date

--- models/Order.js
+++ models/Order.js
@@ -22,10 +22,26 @@ const OrderSchema = new mongoose.Schema({
         enum:['new order', 'cancelled', 'pending', 'delivered'],
         default: 'new order',
     },
-    createdAt:{
+    createdAt: {
         type: Date,
         default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    shippedAt: {
+        type: Date,  // Slaat de tijd op wanneer de order verzonden wordt
+        default: null
     }
+});
+
+OrderSchema.pre('save', function(next) {
+    if (this.isModified('status') && this.status === 'delivered' && !this.shippedAt) {
+        this.shippedAt = Date.now();  // Stel shippedAt in bij statuswijziging naar "delivered"
+    }
+    this.updatedAt = Date.now();  // Werk updatedAt bij bij elke wijziging
+    next();
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
