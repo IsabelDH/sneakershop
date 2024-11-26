5eeb746728d9a01a566c926df10e9b28bf0f3332
IsabelDH <isabel.de.haes@hotmail.com>
2024-11-26 18:04:16 +0100

    update

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../../../controllers/api/v1/user");
-const { checkAuthentication } = require("../../../middleware/authMiddleware");
+//const { checkAuthentication } = require("../../../middleware/authMiddleware");
 
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
