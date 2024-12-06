1e7d65a424148d60ebaf2125c306116d958f895e
IsabelDH <isabel.de.haes@hotmail.com>
2024-12-06 16:12:13 +0100

    feat: change back

--- controllers/api/v1/user.js
+++ controllers/api/v1/user.js
@@ -17,9 +17,6 @@ const login = async (req, res) => {
 
         // Vergelijk het ingevoerde wachtwoord met het gehashte wachtwoord
         const isMatch = await bcrypt.compare(password, user.password);
-        console.log('User password:', user.password);
-console.log('Entered password:', password);
-console.log('Passwords match:', isMatch);
         if (!isMatch) {
             return res.status(401).json({
                 status: "error",
