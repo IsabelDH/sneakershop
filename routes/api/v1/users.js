e1cfb45051d109e4952a0b2c7ce837e8a1f4729d
IsabelDH <isabel.de.haes@hotmail.com>
2024-12-06 15:37:34 +0100

    feat: fixed update admin password?

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../../../controllers/api/v1/user");
+const { checkAuthentication } = require('../../../middleware/authMiddleware');
 
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 router.post("/register", userController.register);
-router.put("/updatepassword", userController.updatepassword);
+
+router.put("/updatepassword", checkAuthentication, userController.updatepassword);
+
 
 module.exports = router;
